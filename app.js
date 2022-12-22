0e02f26f1cf43ac3cd910e9d3961c2ef5f23009e
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2022-12-22 22:04:00 +0600

    fix some error

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const formInput = document.querySelectorAll("form input");
-
+let password = 0;
 formInput[2].addEventListener("keyup", function (e) {
-  const password = e.target.value;
-  if (password.length < 6) {
+  password = e.target.value.length;
+  if (password < 6) {
     this.style.borderColor = "#fa0707";
-    formInput[3].addEventListener("mouseover", function () {
-      console.log(this.style.transform);
-      if (this.style.transform === "translate(400px)") {
-        this.style.transform = "translate(0px)";
-      } else {
-        this.style.transform = "translate(400px)";
-      }
-    });
   } else {
-    this.style.borderColor = "#ddd";
+    this.style.borderColor = "#218f28";
+  }
+});
+
+formInput[3].addEventListener("mouseover", function () {
+  if (password < 6) {
+    if (this.style.transform === "translate(400px)") {
+      this.style.transform = "translate(0px)";
+    } else {
+      this.style.transform = "translate(400px)";
+    }
+  } else {
+    this.style.transform = "translate(0px)";
   }
 });
